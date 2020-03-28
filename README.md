477ff0d58edc26ea414b7027e7720a78e9335c69
Gabriel <brhbs0330@ambev.com.br>
2020-03-28 17:21:53 -0300

    init commit

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ## Create container rabbit
 
 > docker run -d --hostname my-rabbit --name some-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+
+> **Note:** Default user: guest and default pass: guest
---
+++ RabbitMQSend/RabbitMQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Cliente",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "teste 123";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "Cliente",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
